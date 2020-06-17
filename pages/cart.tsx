4eba7a18d3393dbb90fd0213103e90b2edb0ac1f
Jonas Artmeier <jonas-artmeier@web.de>
2020-06-17 18:01:39 +0200

    added ga & gtm

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -111,9 +111,36 @@ export default function cart(props: cartProps) {
   return (
     <div>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W2J294T');`,
+          }}
+        ></script>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=UA-169796110-1`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-169796110-1', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+          }}
+        />
         <title>Jo's Shop</title>
         <link rel="icon" href="/Logo.ico" />
       </Head>
+
       <Header />
       <main>
         <div className="titl">
--- pages/products.js
+++ pages/products.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 
@@ -8,6 +8,32 @@ export default function Products() {
   return (
     <div className="container">
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W2J294T');`,
+          }}
+        ></script>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=UA-169796110-1`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-169796110-1', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+          }}
+        />
         <title>Jo's Shop</title>
         <link rel="icon" href="/Logo.ico" />
       </Head>
@@ -15,10 +41,18 @@ export default function Products() {
       <main>
         <div>
           <Link href="/boys">
-            <img alt="male-biker" className="male-biker" src="male-biker.png"></img>
+            <img
+              alt="male-biker"
+              className="male-biker"
+              src="male-biker.png"
+            ></img>
           </Link>
           <Link href="/girls">
-            <img alt="girl-biker" className="girl-biker" src="girl-bike.png"></img>
+            <img
+              alt="girl-biker"
+              className="girl-biker"
+              src="girl-bike.png"
+            ></img>
           </Link>
         </div>
       </main>
