54b11da4e830946ae9369c28eb132ab1fa3fa652
Jonas Artmeier <jonas-artmeier@web.de>
2020-06-17 17:05:59 +0200

    Add GTM

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W2J294T');`,
+          }}
+        ></script>
         <title>Jo's Shop</title>
         <link rel="icon" href="/Logo.ico" />
       </Head>
